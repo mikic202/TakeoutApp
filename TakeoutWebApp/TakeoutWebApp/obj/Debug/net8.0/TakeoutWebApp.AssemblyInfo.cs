d4e5a67618c0896c4072adb544803bbe8ef973a3
mchomans <01168876@pw.edu.pl>
2023-12-14 09:01:25 +0100

    fix some stuff

--- TakeoutWebApp/TakeoutWebApp/obj/Debug/net8.0/TakeoutWebApp.AssemblyInfo.cs
+++ TakeoutWebApp/TakeoutWebApp/obj/Debug/net8.0/TakeoutWebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TakeoutWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ab3ed3fda363897f5d935221fd1725b6b3bc893")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+457b335afce7d7d550528a73d271a61f6a932950")]
 [assembly: System.Reflection.AssemblyProductAttribute("TakeoutWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TakeoutWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
