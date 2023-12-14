457b335afce7d7d550528a73d271a61f6a932950
mchomans <01168876@pw.edu.pl>
2023-12-14 08:55:33 +0100

    add loging screen

---
+++ TakeoutWebApp/TakeoutWebApp/Shared/User.cs
@@ -0,0 +1,10 @@
+﻿namespace TakeoutWebApp.Shared
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+
+        public string Password { get; set; }  // zazwyczaj hasło ma inny typ
+    }
+}
