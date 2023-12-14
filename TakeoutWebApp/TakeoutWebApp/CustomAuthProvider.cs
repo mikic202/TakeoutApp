1abf38912569cf22281279108643f787d95e87a8
mchomans <01168876@pw.edu.pl>
2023-12-14 21:45:06 +0100

    add sign in screen

---
+++ TakeoutWebApp/TakeoutWebApp/CustomAuthProvider.cs
@@ -0,0 +1,33 @@
+﻿using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace TakeoutWebApp
+{
+	public class CustomAuthProvider : AuthenticationStateProvider
+	{
+		private readonly ILocalStorageService _localStoarage;
+
+		public CustomAuthProvider(ILocalStorageService localStoarage) {
+			this._localStoarage=localStoarage;
+		}
+		
+		public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+		{
+			var state = new AuthenticationState(new System.Security.Claims.ClaimsPrincipal());
+
+			string username = await _localStoarage.GetItemAsStringAsync("username");
+			if (!string.IsNullOrEmpty(username))
+			{
+				var identity = new ClaimsIdentity(new[]
+				{
+					new Claim(ClaimTypes.Name, username)
+				}, "test authentication type");
+
+				state = new AuthenticationState(new System.Security.Claims.ClaimsPrincipal(identity));
+			}
+			NotifyAuthenticationStateChanged(Task.FromResult(state));
+			return state;
+		}
+	}
+}
--- TakeoutWebApp/TakeoutWebApp/Program.cs
+++ TakeoutWebApp/TakeoutWebApp/Program.cs
@@ -1,3 +1,5 @@
+using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using TakeoutWebApp;
@@ -7,5 +9,10 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddOptions();
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthProvider>();
 
 await builder.Build().RunAsync();
