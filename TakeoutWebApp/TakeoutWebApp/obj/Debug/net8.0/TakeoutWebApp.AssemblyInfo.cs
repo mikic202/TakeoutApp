ca4ac5c1f142e30aac5d9c7223cd3fbb91587462
mchomans <01168876@pw.edu.pl>
2023-12-16 10:54:12 +0100

    fully finish ogging screen

--- TakeoutWebApp/TakeoutWebApp/obj/Debug/net8.0/TakeoutWebApp.AssemblyInfo.cs
+++ TakeoutWebApp/TakeoutWebApp/obj/Debug/net8.0/TakeoutWebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TakeoutWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4e5a67618c0896c4072adb544803bbe8ef973a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1abf38912569cf22281279108643f787d95e87a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("TakeoutWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TakeoutWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
