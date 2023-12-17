92144e535f7a84a636cf0ef6500b526663307da9
mchomans <01168876@pw.edu.pl>
2023-12-17 20:18:19 +0100

    add grpcserver class

---
+++ GRPC_Server/GRPC_Server/SignInServer.cs
@@ -0,0 +1,19 @@
+﻿using Grpc.Core;
+using Signin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GRPC_Server
+{
+	public class SignInServer : LoggingInProvider.LoggingInProviderBase
+	{
+		public override Task<SigninReply> Signin(SigninRequest request, ServerCallContext context)
+		{
+			Console.WriteLine("Got message");
+			return Task.FromResult(new SigninReply {Outcome = true });
+		}
+	}
+}
