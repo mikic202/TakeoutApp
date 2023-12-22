c13fc822f7e379b83ad204500f1031da633452d6
mchomans <01168876@pw.edu.pl>
2023-12-22 21:18:04 +0100

    fix newly added structure

--- GRPC_Server/GRPC_Server/obj/Debug/net8.0/GRPC_Server.AssemblyInfo.cs
+++ GRPC_Server/GRPC_Server/obj/Debug/net8.0/GRPC_Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GRPC_Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6682d3adc8b768672bac1fc1b884403c6a258430")]
 [assembly: System.Reflection.AssemblyProductAttribute("GRPC_Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GRPC_Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
