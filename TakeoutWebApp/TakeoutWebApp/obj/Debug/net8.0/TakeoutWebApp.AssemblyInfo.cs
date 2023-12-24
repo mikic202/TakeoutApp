8b414acdd7c87ea2a816f9b272f3b208ceb7df64
mikic202 <mikolaj.chomanski@gmail.com>
2023-12-24 14:35:30 +0100

    add creating new dishes

--- TakeoutWebApp/TakeoutWebApp/obj/Debug/net8.0/TakeoutWebApp.AssemblyInfo.cs
+++ TakeoutWebApp/TakeoutWebApp/obj/Debug/net8.0/TakeoutWebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TakeoutWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62c01922417c63aa90312784abd914c039098d44")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3a8bbdf62b9035b11a3efe60a497e38a75fce44")]
 [assembly: System.Reflection.AssemblyProductAttribute("TakeoutWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TakeoutWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
