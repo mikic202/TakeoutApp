13b1d1a843b456447df9ee487cc21ef1fd3ab4ec
mikic202 <mikolaj.chomanski@gmail.com>
2023-12-24 10:44:54 +0100

    add dish class

--- TakeoutWebApp/TakeoutWebApp/obj/Debug/net8.0/TakeoutWebApp.AssemblyInfo.cs
+++ TakeoutWebApp/TakeoutWebApp/obj/Debug/net8.0/TakeoutWebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TakeoutWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a4495790152d1ecd9cdb5c8edbb53373f2d48d7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("TakeoutWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TakeoutWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
