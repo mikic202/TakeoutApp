5a865f324f56591308bf72ed48fe3bbf4050600e
mikic202 <mikolaj.chomanski@gmail.com>
2023-12-31 21:50:54 +0100

    change app logog

--- TakeoutWebApp/TakeoutWebApp/obj/Debug/net8.0/TakeoutWebApp.AssemblyInfo.cs
+++ TakeoutWebApp/TakeoutWebApp/obj/Debug/net8.0/TakeoutWebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TakeoutWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b414acdd7c87ea2a816f9b272f3b208ceb7df64")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("TakeoutWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TakeoutWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
