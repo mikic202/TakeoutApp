b55b7bf4dc4a2d7ce9aea4b8295e71d61b3618b7
mikic202 <mikolaj.chomanski@gmail.com>
2024-01-03 12:50:42 +0100

    add repeated data update

--- TakeoutWebApp/TakeoutWebApp/Program.cs
+++ TakeoutWebApp/TakeoutWebApp/Program.cs
@@ -6,25 +6,18 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using TakeoutWebApp;
 using Grpc.Net.Client.Web;
-//using Grpc.AspNetCore;
 using static TakeoutWebApp.Pages.Orders;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-//builder.Services.AddGrpc();
-
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddSingleton(services =>
 {
-	Console.WriteLine("Log me in!");
 	var httpHandler = new GrpcWebHandler(GrpcWebMode.GrpcWebText, new HttpClientHandler());
-	Console.WriteLine("Log me in!");
 	var config = services.GetRequiredService<IConfiguration>();
-	Console.WriteLine("Log me in!");
 	var backendUrl = "http://localhost:10001";
-	Console.WriteLine("Log me in!");
 	return GrpcChannel.ForAddress(backendUrl, new GrpcChannelOptions { HttpHandler = httpHandler });
 });
 
