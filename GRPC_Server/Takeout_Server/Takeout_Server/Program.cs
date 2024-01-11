05efdc3778eb366789446c511b94d8db547bb885
mikic202 <mikolaj.chomanski@gmail.com>
2024-01-11 21:09:35 +0100

    fix server issues and test the connection

--- GRPC_Server/Takeout_Server/Takeout_Server/Program.cs
+++ GRPC_Server/Takeout_Server/Takeout_Server/Program.cs
@@ -1,17 +1,25 @@
 using Takeout_Server.Services;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddCors(o => o.AddDefaultPolicy(builder =>
+{
+	builder.AllowAnyOrigin()
+		   .AllowAnyMethod()
+		   .AllowAnyHeader()
+		   .WithExposedHeaders("Grpc-Status", "Grpc-Message", "Grpc-Encoding", "Grpc-Accept-Encoding");
+}));
 
-// Additional configuration is required to successfully run gRPC on macOS.
-// For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
-
-// Add services to the container.
 builder.Services.AddGrpc();
 
 var app = builder.Build();
+
+app.UseCors();
+app.UseGrpcWeb(new GrpcWebOptions { DefaultEnabled = true });
 
 // Configure the HTTP request pipeline.
 app.MapGrpcService<TakeoutServer>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
+
+
